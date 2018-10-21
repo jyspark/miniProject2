ee0dc65b81b1a2dd5b2bf644443425d2030434d9
jyspark <pys4330@gmail.com>
2018-10-21 19:03:58 -0400

    final commit

--- modeling.py
+++ modeling.py
@@ -26,28 +26,9 @@ def modeling_plotting(tr_img,tr_lbl,testing_data):
 	model.add(Dense(2,activation='softmax'))			
 
 
-	model.compile(optimizer= Adam(lr=1e-3), loss='categorical_crossentropy',metrics=['accuracy'])
-	fitting = model.fit(x=tr_img,y=tr_lbl,epochs=80,batch_size=100) #, validation_split=0.2)	 
+	model.compile(optimizer= Adam(lr=2e-8), loss='categorical_crossentropy',metrics=['accuracy'])
+	model.fit(x=tr_img,y=tr_lbl,epochs=80,batch_size=100, validation_split=0.1)	 
 	model.summary()
-
-	'''plt.figure()
-	plt.plot(fitting.history['acc'])
-	plt.plot(fitting.history['val_acc'])
-	plt.title('Model Accuracy')
-	plt.ylabel('Acc')
-	plt.xlabel('Epoch')
-	plt.legend(['Train', 'Test'], loc='upper left')
-	plt.savefig('Model Accuracy.png')
-
-	plt.figure()
-	plt.plot(fitting.history['loss'])
-	plt.plot(fitting.history['val_loss'])
-	plt.title('Model Loss')
-	plt.ylabel('Loss')
-	plt.xlabel('Epoch')
-	plt.legend(['Train', 'Test'], loc='upper left')
-	plt.savefig('Model Loss.png')'''
-
 
 	fig=plt.figure(figsize=(14,14))
 
